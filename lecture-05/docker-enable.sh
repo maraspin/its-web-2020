2ce0f910f4a2bbfb1bcc0ae9b289fca070f31db2
Steve Maraspin <s.maraspin@mvlabs.it>
2021-05-17 23:45:26 +0200

    removed unused material

--- lecture-05/docker-enable.sh
+++
@@ -1,6 +0,0 @@
-/usr/bin/systemctl enable docker
-/usr/bin/systemctl disable nginx
-/usr/bin/systemctl disable apache2
-/usr/bin/apt install docker-compose
-sudo /usr/sbin/groupadd docker
-sudo /usr/sbin/usermod -aG docker mvlabs
